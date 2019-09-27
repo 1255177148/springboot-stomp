e6d86749618a6f6929a5060a2e00465af316aa72
elvis.he <elvis.he@gaiaworks.cn>
2019-09-27 22:15:35 +0800

    获取用户信息的代码更新

--- src/main/java/com/websocket/demo/interceptor/UserInterceptor.java
+++ src/main/java/com/websocket/demo/interceptor/UserInterceptor.java
@@ -3,14 +3,11 @@ package com.websocket.demo.interceptor;
 import com.websocket.demo.entity.FastPrincipal;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 
-import java.util.LinkedList;
-import java.util.Map;
 
 /**
  * @Author hezhan
@@ -25,14 +22,8 @@ public class UserInterceptor implements ChannelInterceptor {
     public Message<?> preSend(Message<?> message, MessageChannel channel){
         StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
         if (StompCommand.CONNECT.equals(accessor.getCommand())){
-            Object raw = message.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
-            if (raw instanceof Map){
-                Object name = ((Map)raw).get("name");
-                if (name instanceof LinkedList){
-                    //设置当前访问器的认证用户
-                    accessor.setUser(new FastPrincipal(((LinkedList)name).get(0).toString()));
-                }
-            }
+            String userName = accessor.getNativeHeader("name").get(0);
+            accessor.setUser(new FastPrincipal(userName));
         }
         return message;
     }
